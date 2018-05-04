990efb078b2fd4d6a33d186c83b3e940fa1fb251
Robbie Alexander <robbie.alexander@gmail.com>
2018-05-04 19:47:23 -0300

    Added export and loading of data and ability to properly call book journal entries from other programs.

--- acct.py
+++ acct.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import sqlite3
+from time import strftime, localtime
 
 conn = sqlite3.connect('acct.db')
 
 DISPLAY_WIDTH = 98
 pd.set_option('display.width',DISPLAY_WIDTH)
 
-def test_je(): # Test
-	data = [2,1,'2018-05-02','Test JE 3','Visa','Food',10]
-	ledger.journal_entry(data)
-
-class Accounts(object): # TODO Cleanup
-	# TODO On load create empty table if it does not exist. Like ledger
+class Accounts(object):
 	def __init__(self):
 		try:
 			self.df = pd.read_sql_query('SELECT * FROM accounts;', conn)
@@ -20,7 +16,7 @@ class Accounts(object): # TODO Cleanup
 
 	def printAccts(self):
 		print (self.df)
-		print ('-' * DISPLAY_WIDTH+'\n')
+		print ('-' * DISPLAY_WIDTH)
 
 	def load_accts(self):
 		infile = input('Enter a filename: ')
@@ -28,11 +24,13 @@ class Accounts(object): # TODO Cleanup
 			self.df = pd.read_csv(f)
 		self.df.to_sql('accounts', conn)
 		self.printAccts()
-
+	
 class Ledger(object):
-	ledger_name = input('Enter a name for the ledger: ')
-	#ledger_name = 'test_1'	#debug
-	def __init__(self):
+	def __init__(self, ledger_name=None):
+		if ledger_name == None:
+			self.ledger_name = input('Enter a name for the ledger: ')
+		else:
+			self.ledger_name = ledger_name
 		self.create_table()
 		self.refresh_data()
 			
@@ -42,7 +40,7 @@ class Ledger(object):
 				txn_id INTEGER PRIMARY KEY,
 				event_id integer NOT NULL,
 				entity_id integer NOT NULL,
-				transaction_date date NOT NULL,
+				date date NOT NULL,
 				description text,
 				debit_acct text NOT NULL,
 				credit_acct text NOT NULL,
@@ -57,78 +55,87 @@ class Ledger(object):
 
 	def printGL(self):
 		print (self.df)
-		print ('-' * DISPLAY_WIDTH+'\n')
-		for column in self.df:
-			print (self.df[column].dtype)
-		print()
+		print ('-' * DISPLAY_WIDTH)
 
 	def refresh_data(self):
 		self.df = pd.read_sql_query('SELECT * FROM ' + self.ledger_name + ';', conn, index_col='txn_id')
 
 	def journal_entry(self, journal_data = None):
+		cur = conn.cursor()
 		if journal_data is None:
 			# TODO Validation
-			event = str(1) # TODO if blank make copy of rowid value
+			event = input('Enter the Event_ID or press enter if unknown: ')
+			#if event == "":
+			#	event = last_insert_rowid(conn) + 1 # TODO fix this
+			#	print(event) #debug
 			entity = input('Enter the Entity_ID: ')
-			date = str(pd.to_datetime(input('Enter a date as format yyyy-mm-dd: '), format='%Y-%m-%d').date())
+			#while True:
+			#	try: # TODO make into while loop
+			date_raw = input('Enter a date as format yyyy-mm-dd: ')
+			#		if len(date) < 10:
+			#			continue
+			date = str(pd.to_datetime(date_raw, format='%Y-%m-%d').date())
+			#		break
+			#	except:
+			#		print('Try again with proper format yyyy-mm-dd: ')
+			#		continue
 			desc = input('Enter a description: ')
 			debit = input('Enter the account to debit: ')
+			#while debit not in accts.df['accounts']:
+			#	debit = input(debit + ' does not exist. Enter the account to debit: ') # TODO make into while loop
 			credit = input('Enter the account to credit: ')
 			amount = input('Enter the amount: ')
 			
-			#event = str(1) # TODO if blank make copy of rowid value
-			#entity = str(1) # input('Enter the Entity_ID: ')
-			#date = str(pd.to_datetime('2018-04-08', format='%Y-%m-%d')) # pd.to_datetime(input('Enter a date as format yyyy-mm-dd: '), format='%Y-%m-%d')
-			#desc = 'Test JE 1' # input('Enter a description: ')
-			#debit = 'Savings Expense' # input('Enter the account to debit: ')
-			#credit = 'Chequeing' # input('Enter the account to credit: ')
-			#amount = str(100.00) # input('Enter the amount: ')
-		else:
-			event = str(journal_data[0])
-			entity = str(journal_data[1])
-			date = str(journal_data[2])
-			desc = str(journal_data[3])
-			debit = str(journal_data[4])
-			credit = str(journal_data[5])
-			amount = str(journal_data[6])
-
-		print(event+entity+date+desc+debit+credit+amount+'\n')
-		values = (event,entity,date,desc,debit,credit,amount)
+			values = (event,entity,date,desc,debit,credit,amount)
+			cur.execute('INSERT INTO ' + self.ledger_name + ' VALUES (NULL,?,?,?,?,?,?,?)', values)
 			
-		cur = conn.cursor()
-		cur.execute('INSERT INTO '+self.ledger_name+' VALUES (NULL,?,?,?,?,?,?,?)', values)
+		else:
+			for je in journal_data:
+				event = str(je[0])
+				entity = str(je[1])
+				date = str(je[2])
+				desc = str(je[3])
+				debit = str(je[4])
+				credit = str(je[5])
+				amount = str(je[6])
+				print(je)
+
+				values = (event,entity,date,desc,debit,credit,amount)
+				cur.execute('INSERT INTO ' + self.ledger_name + ' VALUES (NULL,?,?,?,?,?,?,?)', values)
+
 		conn.commit()
 		cur.close()
 		self.refresh_data()
 
 	def sanitize_ledger(self):
-		self.df.query('Debit_Acct or Credit_Acct != Admin') # Fix this
-		self.df.drop_duplicates() # Test this
-
-	def load_data(self): # TODO Finish this
+		self.df.query('Debit_Acct or Credit_Acct != Admin') # TODO Fix this
+		self.df.drop_duplicates() # TODO Test this
+		
+	def load_data(self):
 		infile = input('Enter a filename: ')
-		ledger_name = input('Enter a name for the ledger: ')
 		with open(infile, 'r') as f:
-			self.df = pd.read_csv(f)
-			self.df.set_index('txn_id', inplace=True)
+			load_df = pd.read_csv(f)
+			load_df.set_index('txn_id', inplace=True)
+			lol = load_df.values.tolist()
+			print(load_df)
+			print ('-' * DISPLAY_WIDTH)
+			self.journal_entry(lol)
 			#self.sanitize_ledger()
-			#self.df.to_sql(ledger_name, conn, if_exists="replace")
-
-		self.printGL()
 
 	def exportGL(self):
-		outfile = input('Enter a filename to save as: ')
-		save_location = "/data/"
+		outfile = self.ledger_name + strftime('_%Y-%m-%d', localtime()) + '.csv'
+		save_location = 'data/'
 		self.df.to_csv(save_location + outfile)
+		print('File saved as ' + save_location + outfile + '\n')
 
-	# TODO credits = df.groupby('Credit').sum()
-
+	#credits = df.groupby('Credit').sum() # TODO Use this for the B/S and I/S
+		
 if __name__ == '__main__':
-	ledger = Ledger()
+	ledger = Ledger('test_1')
 	accts = Accounts()
 
 	while True:
-		command = input('Type one of the following commands:\nprintGL, JE, loadData, printAccts, loadAccts, exit\n')
+		command = input('Type one of the following commands:\nprintGL, JE, loadData, exportGL, printAccts, loadAccts, exit\n')
 		if command.upper() == "EXIT":
 			exit()
 		elif command.upper() == "PRINTGL":
@@ -145,7 +152,5 @@ if __name__ == '__main__':
 			ledger.journal_entry()
 		elif command.upper() == "SANITIZE":
 			ledger.sanitize_ledger()
-		elif command.upper() == "TESTJE": #debug
-			test_je()
 		else:
 			print('Not a valid command. Type exit to close.')
