004dfde4e1ea5379f0a2b703cbfd5d359b897099
Robbie Alexander <robbie.alexander@gmail.com>
2024-08-04 20:05:46 -0300

    Added rich color and map loading and move cost

--- move.py
+++ move.py
@@ -3,34 +3,112 @@
 import pandas as pd
 import argparse
 import random
-# from rich import print
+from rich import print
 
 MAP_SIZE = 4 #64
 
 class Map:
     def __init__(self, map_size):
+        if args.map is None:
+            self.gen_map(map_size)
+        else:
+            self.map_gen_file()
+        self.display_map()
+        # print(f'world_map created:\n{self}')
+
+    def gen_map(self, map_size):
         self.map_size = map_size
         if isinstance(self.map_size, int):
             self.map_size = (self.map_size, self.map_size)
         elif len(self.map_size) == 1:
             self.map_size = (self.map_size[0], self.map_size[0])
-
         print('map_size:', self.map_size)
         self.world_map = [[dict() for _ in range(self.map_size[0])] for _ in range(self.map_size[1])]
-        print('world_map 1:', self.world_map)
-
-        self.get_terrain()
-
+        # print('world_map 1:', self.world_map)
+        self.get_terrain_data()
         for row in self.world_map:
             for tile in row:
                 terrain_select = random.choices(self.terrain_items['item_id'].tolist(), self.terrain_items['coverage'].tolist())[0]
                 tile.update({'terrain': Tile(terrain_select, self.terrain_items)})
         # self.world_map = pd.DataFrame(self.world_map) # Replace pandas here
         # self.world_map.applymap(lambda d: d.update({'terrain': Tile()})) # Replace pandas here
-        self.display_map()
-        print('world_map created:\n', self)
+        return self.world_map
+    
+    def map_gen_file(self, infile='data/map01.csv'):
+        if args.map is not None:
+            infile = args.map
+            if '.csv' not in infile:
+                infile = infile + '.csv'
+            if 'data/' not in infile:
+                infile = 'data/' + infile
+            print('infile:', infile)
+        with open(infile, 'r') as f:
+            map_data = pd.read_csv(f, header=None)
+        self.map_size = map_data.shape
+        print('map_size:', self.map_size)
+        self.world_map = [[dict() for _ in range(self.map_size[0])] for _ in range(self.map_size[1])]
+        # print('world_map 1:', self.world_map)
+        self.get_terrain_data()
+        for i, row in map_data.iterrows():
+            for j, tile in enumerate(row):
+                if tile == '.':
+                    terrain_select = 'Grassland'
+                elif tile == '#':
+                    terrain_select = 'Arable Land'
+                elif tile == 'F':
+                    terrain_select = 'Forest'
+                elif tile == 'R':
+                    terrain_select = 'Rocky Land'
+                elif tile == 'M':
+                    terrain_select = 'Mountain'
+                elif tile == 'W':
+                    terrain_select = 'Wetlands'
+                else:
+                    terrain_select = 'Grassland'
+                self.world_map[i][j].update({'terrain': Tile(terrain_select, self.terrain_items)})
+        return self.world_map
 
-    def get_terrain(self, infile='data/items.csv'):
+    def save_map(self, filename=None):
+        if filename is None:
+            filename = input('Enter filename: ')
+            if '.csv' not in filename:
+                filename = filename + '.csv'
+            if 'data/' not in filename:
+                filename = 'data/' + filename
+        print('Save filename: ', filename)
+        df = pd.DataFrame(self.display_map)
+        print(df)
+        df.to_csv(filename, index=False, header=False)
+
+    def edit_terrain(self, pos=None, terrain=None):
+        if pos is None:
+            x = input('Enter x coord: ')
+            y = input('Enter y coord: ')
+            try:
+                pos = (int(y), int(x))
+            except ValueError:
+                print('Enter whole numbers only, try again.')
+                return
+        if terrain is None:
+            terrain = input('Enter terrain: ')
+        self.world_map[pos[0]][pos[1]].update({'terrain': Tile(terrain, self.terrain_items)})
+        if terrain == 'Grassland': # TODO Could likely do this more efficiently
+            icon = '[bright_green].[/bright_green]'
+        elif terrain == 'Arable Land':
+            icon = '[yellow]#[/yellow]'
+        elif terrain == 'Forest':
+            icon = '[green]F[/green]'
+        elif terrain == 'Rocky Land':
+            icon = '[grey62]R[/grey62]'
+        elif terrain == 'Mountain':
+            icon = '[red]M[/red]'
+        elif terrain == 'Wetlands':
+            icon = '[cyan]W[/cyan]'
+        else:
+            icon = ','
+        self.display_map[pos[0]][pos[1]] = icon
+
+    def get_terrain_data(self, infile='data/items.csv'):
         # Note: int_rate_var is the column name for units of land.
         with open(infile, 'r') as f:
             self.terrain_items = pd.read_csv(f, keep_default_na=False, comment='#')
@@ -45,22 +123,22 @@ class Map:
         self.display_map = [[None for _ in range(self.map_size[0])] for _ in range(self.map_size[1])]
         for i, row in enumerate(self.world_map):
             for j, tile in enumerate(row):
-                tile = tile.get('terrain')
-                if tile.terrain == 'Grassland': # TODO Could likely do this more efficiently
-                    tile = '.'
-                elif tile.terrain == 'Arable Land':
-                    tile = '#'
-                elif tile.terrain == 'Forest':
-                    tile = 'F'
-                elif tile.terrain == 'Rocky Land':
-                    tile = 'R'
-                elif tile.terrain == 'Mountain':
-                    tile = 'M'
-                elif tile.terrain == 'Wetlands':
-                    tile = 'W'
+                terrain = tile.get('terrain').terrain
+                if terrain == 'Grassland': # TODO Could likely do this more efficiently
+                    icon = '[bright_green].[/bright_green]'
+                elif terrain == 'Arable Land':
+                    icon = '[yellow]#[/yellow]'
+                elif terrain == 'Forest':
+                    icon = '[green]F[/green]'
+                elif terrain == 'Rocky Land':
+                    icon = '[grey62]R[/grey62]'
+                elif terrain == 'Mountain':
+                    icon = '[red]M[/red]'
+                elif terrain == 'Wetlands':
+                    icon = '[cyan]W[/cyan]'
                 else:
-                    tile = ','
-                self.display_map[i][j] = tile
+                    icon = ','
+                self.display_map[i][j] = icon
 
     def __str__(self):
         # self.map_display = '\n'.join(['\t'.join([str(tile) for tile in row]) for row in self.world_map])
@@ -76,6 +154,10 @@ class Tile:
     def __init__(self, terrain='Land', terrain_items=None):
         self.terrain = terrain
         self.move_cost = terrain_items[terrain_items['item_id'] == self.terrain]['int_rate_fix'].values[0]
+        if self.move_cost == 'None':
+            self.move_cost = None
+        if self.move_cost is not None:
+            self.move_cost = int(self.move_cost)
 
     def __str__(self):
         return self.terrain
@@ -86,7 +168,8 @@ class Tile:
 class Player:
     def __init__(self, name, world_map, icon='P'):
         self.name = name
-        self.icon = icon
+        self.icon = '[blink]' + icon + '[/blink]'
+        print('player icon:', self.icon)
         self.pos = (0, 0) # Start position
         world_map.world_map[self.pos[0]][self.pos[1]]['Agent'] = self.name
         world_map.display_map[self.pos[0]][self.pos[1]] = self.icon
@@ -96,16 +179,21 @@ class Player:
 
     def reset_moves(self):
         self.remain_move = self.movement
+        print('Moves reset.')
 
     def move(self):
         self.old_pos = self.pos
         print('Position:', self.old_pos)
         if self.get_move() is None: # TODO This isn't very clear
             return
+        if not self.calc_move(self.pos):
+            print(f'Not enough movement to enter tile. Movement Remaining: {self.remain_move}/{self.movement}')
+            self.pos = self.old_pos
+            return
         try:
             world_map.world_map[self.pos[0]][self.pos[1]]['Agent'] = self.name
             world_map.display_map[self.pos[0]][self.pos[1]] = self.icon
-            world_map.display_map[self.old_pos[0]][self.old_pos[1]] = 'X'
+            world_map.display_map[self.old_pos[0]][self.old_pos[1]] = self.get_terrain(self.old_pos)
             del world_map.world_map[self.old_pos[0]][self.old_pos[1]]['Agent']
             # world_map.world_map.at[self.pos[0], self.pos[1]]['Agent'] = self.name # Replace pandas here
             # del world_map.world_map.at[self.old_pos[0], self.old_pos[1]]['Agent'] # Replace pandas here
@@ -113,10 +201,36 @@ class Player:
             print('Out of bounds, try again.')
             self.pos = self.old_pos
             return
-        print('end')
+        print('End move.')
 
-    def calc_move():
-        pass
+    def get_terrain(self, pos):
+        terrain = world_map.world_map[pos[0]][pos[1]]['terrain'].terrain
+        if terrain == 'Grassland': # TODO Could likely do this more efficiently
+            icon = '[bright_green].[/bright_green]'
+        elif terrain == 'Arable Land':
+            icon = '[yellow]#[/yellow]'
+        elif terrain == 'Forest':
+            icon = '[green]F[/green]'
+        elif terrain == 'Rocky Land':
+            icon = '[grey62]R[/grey62]'
+        elif terrain == 'Mountain':
+            icon = '[red]M[/red]'
+        elif terrain == 'Wetlands':
+            icon = '[cyan]W[/cyan]'
+        else:
+            icon = ','
+        return icon
+
+    def calc_move(self, pos):
+        target_terrain = world_map.world_map[pos[0]][pos[1]]['terrain']
+        if target_terrain.move_cost is None:
+            print(f'Cannot cross {target_terrain}.')
+            return
+        print('remain_move:', self.remain_move)
+        print('target_terrain.move_cost:', target_terrain.move_cost)
+        if self.remain_move >= target_terrain.move_cost:
+            self.remain_move -= target_terrain.move_cost
+            return True
 
     def get_move(self):
         print('\nEnter "exit" to exit.')
@@ -128,6 +242,18 @@ class Player:
             return
         if key == 'terrain':
             print('Display terrain item details:\n', world_map.terrain_items)
+            self.pos = self.old_pos
+            return
+        if key == 'r' or key == 'reset':
+            self.reset_moves()
+            self.pos = self.old_pos
+            return
+        if key == 'edit':
+            world_map.edit_terrain()
+            self.pos = self.old_pos
+            return
+        if key == 'save':
+            world_map.save_map()
             self.pos = self.old_pos
             return
         elif key == 'w':
@@ -161,11 +287,17 @@ class Player:
     def __repr__(self):
         return self.name
 
+def setup():
+    # TODO Move class instantiation here
+    pass
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--size', type=str, help='The map size as either a list of two numbers or one for square.')
     parser.add_argument('-i', '--items', type=str, help='The name of the items csv config file.')
     parser.add_argument('-r', '--seed', type=str, default=11, help='Set the seed for the randomness.')
+    parser.add_argument('-m', '--map', type=str, help='The name of the map csv data file.')
+    parser.add_argument('-p', '--players', type=int, default=1, help='The number of players in the world.')
     args = parser.parse_args()
 
     if args.seed:
@@ -179,12 +311,21 @@ if __name__ == '__main__':
     
 
     world_map = Map(MAP_SIZE)
-    player = Player('Player 1', world_map)
+    players = []
+    for player_num in range(args.players):
+        player_name = 'Player ' + str(player_num+1)
+        player = Player(player_name, world_map, str(player_num+1))
+        players.append(player)
+    print(f'Players:\n{players}')
     print(f'world_map:\n{world_map}')
 
     while True:
-        player.move()
-        print(f'Current world map:\n{world_map}')
+        for player in players:
+            while player.remain_move:
+                player.move()
+                print(f'Current world map:\n{world_map}')
+                print(f'{player.name} Movement: {player.remain_move}')
+            player.reset_moves()
 
 ## TODO
 # Display map
@@ -196,4 +337,14 @@ if __name__ == '__main__':
 # Load map
 # Save map
 
+# Multiple Players/Units
+# Don't allove move into unit tile
+# Create tile for player spawn, that isn't visible on the map
+#    Can make a reusable property for items that don't show on map
+# Allow player spawns to be randomly placed and randomly used
+# Make similar to above for mob spawns
+
+# Make togglable options for map wrapping
+
+# scp data/map02.csv robale5@becauseinterfaces.com:/home/robale5/becauseinterfaces.com/acct/data
 # scp move.py robale5@becauseinterfaces.com:/home/robale5/becauseinterfaces.com/acct
